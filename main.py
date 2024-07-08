e9b00334711b38b2d91854a22bcc5273c4de51e8
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-07-08 13:11:30 -0400

    Updated main.py to run main function:2

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from .mutual_info_regression.mi_regression_all import mi_regression_all
 from .network.xgboostnet import xgboostnet
 from typing import Optional
 
-def main(calculate_MI: str = False, xgboost_event_index: int = 1, xgboost_specific_gene: Optional[str] = None):
+def main(calculate_MI: str = False, view_or_calc_network: str = True, \
+    xgboost_event_index: int = 1, xgboost_specific_gene: Optional[str] = None, *args, **kwargs):
 
     if calculate_MI == True:
         mi_reg_df = mi_regression_all()
     
-    fit_param = xgboostnet(xgboost_event_index, xgboost_specific_gene)
+    if view_or_calc_network == True:
+        fit_param = xgboostnet(xgboost_event_index, xgboost_specific_gene)
     
 
 if __name__ == "__main__":
