370efa3886a38dbfb13434b29fe681df90cfc1d4
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-07-10 10:10:45 -0400

    Changed data_dir_path script to automatically create subdir if it doesn't exist

--- data_dir_path.py
+++ data_dir_path.py
@@ -6,7 +6,8 @@ def data_dir_path(subdir="processed"):
     
     This function starts by getting the current working directory, then 
     navigates to the parent directory and appends the relative path 'data/<subdir>'
-    to it. The resulting absolute path is returned.
+    to it. If the specified subdirectory does not exist, it creates it.
+    The resulting absolute path is returned.
 
     Args:
         subdir (str): The name of the subdirectory within 'data'. Defaults to 'processed'.
@@ -18,5 +19,8 @@ def data_dir_path(subdir="processed"):
     curr_path = os.getcwd()
     parent_dir = os.path.abspath(os.path.join(curr_path, os.pardir))
     data_path_whole = os.path.join(parent_dir, data_path)
+    
+    # Create the directory if it doesn't exist
+    os.makedirs(data_path_whole, exist_ok=True)
     
     return data_path_whole
