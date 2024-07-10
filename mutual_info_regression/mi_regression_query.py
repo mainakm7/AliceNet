e790ef5c7aead112678a1a925e0c219c172a84fc
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-07-10 12:53:46 -0400

    Updated mi_regression_query script to include a current melt_mi filename function to dynamically update the filenames

--- mutual_info_regression/mi_regression_query.py
+++ mutual_info_regression/mi_regression_query.py
@@ -1,7 +1,19 @@
 import numpy as np
 import pandas as pd
-from typing import List
+from typing import List, Optional
 from ..utils.load_data import load_melted_mi_data
+
+def current_melted_mi_file(filename: Optional[str] = "mutualinfo_reg_one_to_one_MI_all_melted.csv") -> str:
+    """
+    Returns the current filename for the melted mutual information data.
+
+    Args:
+        filename (Optional[str]): The filename of the melted MI data CSV. Defaults to "mutualinfo_reg_one_to_one_MI_all_melted.csv".
+
+    Returns:
+        str: The current filename for the melted mutual information data.
+    """
+    return filename
 
 def mi_regression_query_specific_gene(specific_gene: str = "AR") -> List[str]:
     """
@@ -15,7 +27,7 @@ def mi_regression_query_specific_gene(specific_gene: str = "AR") -> List[str]:
     """
     
     # Load mutual information data
-    mi_data = load_melted_mi_data()
+    mi_data = load_melted_mi_data(current_melted_mi_file())
     
     gene_events = mi_data[mi_data["spliced_genes"] == specific_gene]
     event_list = list(np.unique(gene_events["Splicing events"]))
@@ -35,7 +47,7 @@ def mi_regression_query_specific_event(specific_event: str, specific_gene: str =
     """
     
     # Load mutual information data
-    mi_data = load_melted_mi_data()
+    mi_data = load_melted_mi_data(current_melted_mi_file())
     
     gene_events = mi_data[mi_data.index == specific_gene]
     event_mi = gene_events[gene_events["Splicing events"] == specific_event]
