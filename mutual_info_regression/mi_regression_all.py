f714c43899aee3508f7090ffeaad178aa1051029
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-07-10 13:28:38 -0400

    updated mi_gression_all script to add docstring

--- mutual_info_regression/mi_regression_all.py
+++ mutual_info_regression/mi_regression_all.py
@@ -4,17 +4,35 @@ import pandas as pd
 from joblib import delayed, Parallel
 from datetime import datetime
 import os
-from ..utils.data_matrices import events_mat, genes_mat, sf_events_upd, sf_exp_upd
+from ..utils.data_matrices import sf_events_upd, sf_exp_upd
 from ..utils.data_dir_path import data_dir_path
 
 
 def mi_regression_all():
+    """
+    Perform mutual information regression for all combinations of genes and splicing events,
+    save results to a CSV file.
+
+    Returns:
+        pd.DataFrame: DataFrame containing mutual information regression values.
+    """
+    # Extract column and index names
     cols = sf_events_upd.index
     ind = sf_exp_upd.index
 
     def compute_mutual_info(i, j):
-        event = events_mat[j, :]
-        gene = genes_mat[i, :]
+        """
+        Compute mutual information between a gene and a splicing event.
+
+        Args:
+            i (int): Index of the gene.
+            j (int): Index of the splicing event.
+
+        Returns:
+            float: Mutual information regression value.
+        """
+        event = sf_events_upd.iloc[j, :].values
+        gene = sf_exp_upd.iloc[i, :].values
 
         # Filter out NaNs
         mask = ~np.isnan(event)
@@ -26,25 +44,19 @@ def mi_regression_all():
         return mi_reg_val[0]
 
     # Parallel computation
-    results1 = Parallel(n_jobs=-1)(
+    results = Parallel(n_jobs=-1)(
         delayed(compute_mutual_info)(i, j)
         for i in range(len(sf_exp_upd))
         for j in range(len(sf_events_upd))
     )
 
-    # Create results array
-    mi_reg_parallel = np.zeros((len(sf_exp_upd), len(sf_events_upd)))
-    k = 0
-    for i in range(len(sf_exp_upd)):
-        for j in range(len(sf_events_upd)):
-            mi_reg_parallel[i, j] = results1[k]
-            k += 1
+    # Reshape results into array
+    mi_reg_parallel = np.array(results).reshape(len(sf_exp_upd), len(sf_events_upd))
 
     # Convert to DataFrame
     mi_reg_df = pd.DataFrame(mi_reg_parallel, index=ind, columns=cols)
-    
-    # Define the paths
 
+    # Define the save path using current timestamp
     data_path_whole = data_dir_path()
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     save_path = os.path.join(data_path_whole, f"mi_reg_all_{timestamp}.csv")
@@ -53,4 +65,3 @@ def mi_regression_all():
     mi_reg_df.to_csv(save_path)
 
     return mi_reg_df
-
