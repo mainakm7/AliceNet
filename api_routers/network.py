6241c1f519ad3b4a21e590606034f2ff8caae661
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-07-23 14:09:55 -0400

    Added separate module for heirarchical clustering based on elbow

--- api_routers/network.py
+++ api_routers/network.py
@@ -4,6 +4,7 @@ from typing import Optional, Tuple, List
 from ..network.hyperparameter_tuning import hyperparameter_tuning
 from ..network.data_preparation import data_preparation
 from ..network.xgboostnet import xgboostnet
+from ..clustering.feature_clustering import feature_clustering
 from ..utils.data_loader import sf_events_upd
 import pandas as pd
 import numpy as np
@@ -13,7 +14,6 @@ from ..database import Database
 def get_db():
     db = Database.get_db()
     return db
-
 
 class Hyperparameters(BaseModel):
     n_estimators: Optional[Tuple[int, int]] = (50, 200)
@@ -26,39 +26,36 @@ class Hyperparameters(BaseModel):
     reg_alpha: Optional[Tuple[float, float]] = (1e-3, 1e1)
     reg_lambda: Optional[Tuple[float, float]] = (1e-3, 1e1)
 
-
 class DataParams(BaseModel):
     test_size: Optional[float] = 0.3
-
 
 router = APIRouter(prefix="/network", tags=["Network"])
 
 @router.get("/event_gene_select", status_code=status.HTTP_200_OK)
-async def select_specific_splicedgene() -> list[str]:
+async def select_specific_splicedgene() -> List[str]:
     sf_events_df = sf_events_upd.copy()
     sf_events_df["gene"] = sf_events_df.index.to_series().apply(lambda x: x.split("_")[0])
     return list(np.unique(sf_events_df["gene"]))
 
 @router.get("/specific_event_select/{gene}", status_code=status.HTTP_200_OK)
-async def select_specific_splicedevent(gene: str = Path()) -> list[str]:
+async def select_specific_splicedevent(gene: str = Path(..., description="Gene name")) -> List[str]:
     sf_events_df = sf_events_upd.copy()
     sf_events_df["gene"] = sf_events_df.index.to_series().apply(lambda x: x.split("_")[0])
     return list(sf_events_df[sf_events_df["gene"] == gene].index)
     
-    
-async def get_genes() -> list[str]:
+async def get_genes() -> List[str]:
     return await select_specific_splicedgene()
 
-async def get_events(gene: str) -> list[str]:
+async def get_events(gene: str) -> List[str]:
     return await select_specific_splicedevent(gene)   
-    
+
 @router.post("/data_prepare/{gene}", status_code=status.HTTP_201_CREATED)
 async def data_prepare(
     param: DataParams,
-    gene: str = Path(description="Choose a gene to analyze"),
-    event: str = Query(description="Choose an event of the chosen gene"),
+    gene: str = Path(..., description="Choose a gene to analyze"),
+    event: str = Query(..., description="Choose an event of the chosen gene"),
     genes: List[str] = Depends(get_genes),
-    events: List[str] = Depends(lambda gene: get_events(gene))
+    events: List[str] = Depends(lambda: get_events(gene))
 ):
     if gene not in genes:
         raise HTTPException(status_code=400, detail=f"Gene {gene} is not in the list of available genes.")
@@ -83,7 +80,6 @@ async def data_prepare(
     except IndexError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-
 @router.post("/hptuning", status_code=status.HTTP_201_CREATED)
 def hp_tuning(hparams: Hyperparameters):
     try:
@@ -103,11 +99,10 @@ def hp_tuning(hparams: Hyperparameters):
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An error occurred: {e}")
 
-
 @router.get("/xgboostnetfit/{gene}", status_code=status.HTTP_200_OK)
 async def xgboostnetfit(
-    gene: str = Path(...,description="Choose a gene to analyze"),
-    event: str = Query(...,description="Choose an event of the chosen gene"),
+    gene: str = Path(..., description="Choose a gene to analyze"),
+    event: str = Query(..., description="Choose an event of the chosen gene"),
     db: Database = Depends(get_db),
     genes: List[str] = Depends(get_genes)
 ):
@@ -122,15 +117,14 @@ async def xgboostnetfit(
         raise HTTPException(status_code=400, detail=f"Event {event} is not in the list of available events for gene {gene}.")
 
     try:
-        best_params, final_rmse, shap_df = xgboostnet(event_name=event)
+        best_params, final_rmse = xgboostnet(event_name=event)
 
         # Insert fit params into MongoDB
         db['xgboost_params'].insert_one({
             "spliced_gene": gene,
             "specific_event": event,
             "xgboost_params": best_params,
-            "xgboost_fit_rmse": final_rmse,
-            "xgboost_fit_shap": shap_df.to_dict(orient="split")
+            "xgboost_fit_rmse": final_rmse
         })
 
         return {"best_param": best_params, "final_rmse": final_rmse}
@@ -139,14 +133,12 @@ async def xgboostnetfit(
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An error occurred: {e}")
 
-
 @router.get("/xgboostnetquery/{gene}", status_code=status.HTTP_200_OK)
 async def xgboostnetquery(
     gene: str = Path(..., description="Choose a gene to analyze"),
     event: str = Query(..., description="Choose an event of the chosen gene"),
     db: Database = Depends(get_db)
 ):
-   
     try:
         xgboost_fit = list(db['xgboost_params'].find({
             "spliced_gene": gene,
@@ -158,14 +150,13 @@ async def xgboostnetquery(
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"An error occurred: {e}")
 
-
-@router.get("/cluster/{gene}", status_code=status.HTTP_200_OK)
+@router.post("/cluster/{gene}", status_code=status.HTTP_200_OK)
 async def feature_cluster(
+    num_cluster: int,
     gene: str = Path(..., description="Choose a gene to analyze"),
     event: str = Query(..., description="Choose an event of the chosen gene"),
     genes: List[str] = Depends(get_genes)
 ):
-    
     # Validate gene and event
     if gene not in genes:
         raise HTTPException(status_code=400, detail=f"Gene {gene} is not in the list of available genes.")
@@ -181,12 +172,14 @@ async def feature_cluster(
         response.raise_for_status()
         xgboost_fit_data = response.json()
 
+        adj_matrix_df = pd.DataFrame(
+            xgboost_fit_data[0]["xgboost_fit_adj_matrix"]["data"],
+            index=xgboost_fit_data[0]["xgboost_fit_adj_matrix"]["index"],
+            columns=xgboost_fit_data[0]["xgboost_fit_adj_matrix"]["columns"]
+        )
         
-        clustering_results = {
-            "gene": gene,
-            "event": event,
-            "cluster": ""
-        }
+        clustered_genes = feature_clustering(adj_matrix_whole_df=adj_matrix_df, num_clusters=num_cluster)
+        clustering_results = {f"Cluster {i}": cluster for i, cluster in enumerate(clustered_genes)}
 
         return clustering_results
     except requests.RequestException as e:
--- network/xgboostnet.py
+++ network/xgboostnet.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import numpy as np
-import optuna
 from .custom_model import custom_model
-from .hyperparameter_tuning import hyperparameter_tuning
 from sklearn.metrics import mean_squared_error
 from ..utils.data_dir_path import data_dir_path
-from .data_preparation import data_preparation
-from scipy.cluster.hierarchy import linkage, dendrogram, fcluster, leaves_list
-import shap
-from typing import Tuple, Optional
+from typing import Tuple
 import json
 import os
 import requests
 from fastapi import HTTPException, status
+
+final_model_fit: custom_model
+train_data: pd.DataFrame
 
 def xgboostnet(event_name: str) -> Tuple[dict, float, pd.DataFrame]:
     """
@@ -30,6 +28,7 @@ def xgboostnet(event_name: str) -> Tuple[dict, float, pd.DataFrame]:
         response_data.raise_for_status()
         data = response_data.json()
 
+        # Convert JSON data to DataFrames
         train_X = pd.DataFrame.from_dict(data["train_X"])
         train_y = pd.Series(data["train_y"])
         test_X = pd.DataFrame.from_dict(data["test_X"])
@@ -43,58 +42,27 @@ def xgboostnet(event_name: str) -> Tuple[dict, float, pd.DataFrame]:
         # Get best hyperparameters
         best_params_custom = study_custom["best_params"]
 
-        # Train final model with best hyperparameters
+        # Initialize and train the final model with best hyperparameters
         final_model_custom = custom_model()
-        final_model_custom.pca_fit(train_X, min(10, train_X.shape[1]))  # Assuming train_X is the correct dataframe
+        final_model_custom.pca_fit(train_X, min(10, train_X.shape[1]))  # Perform PCA
         final_model_custom.fit(
             train_X.values, train_y.values,
             params=best_params_custom, random_state=42, apply_pca=True, verbose=False, n_splits=5
         )
 
-        # Evaluate final model
+        # Evaluate the final model
         final_preds_custom = final_model_custom.predict(test_X.values)
         final_rmse_custom = mean_squared_error(test_y.values, final_preds_custom) ** 0.5
 
-        # Calculating SHAP values for the model
-        train_X_transformed = final_model_custom.pca_transform(train_X)
-
-        explainer_custom = shap.TreeExplainer(final_model_custom.xgb_model, train_X_transformed)
-        shap_values_custom = explainer_custom.shap_values(train_X_transformed)
-        shap_values_custom_abs = np.abs(shap_values_custom)
-        loadings = final_model_custom.pca_model.components_
-        num_principal_components = loadings.shape[0]
-        pc_name_list = ["PC" + str(i + 1) for i in range(num_principal_components)]
-        shap_df_custom_abs2 = pd.DataFrame(shap_values_custom_abs, columns=pc_name_list)
+        global final_model_fit, train_data
         
-        # Clustering the features for individual PCs and forming an adjacency matrix
-        adj_matrix_whole_df = pd.DataFrame(np.zeros((train_X.shape[1], train_X.shape[1])), index=train_X.columns, columns=train_X.columns)
-
-        for i, pci in enumerate(pc_name_list):
-            pcshap = shap_df_custom_abs2[pci].copy()
-            pcshap  = pcshap.values.reshape(-1, 1)
-            pcloading = loadings[i, :].copy()
-            pcloading = pcloading.reshape(1, -1)
-            PC_sf_shap = np.dot(pcshap, pcloading)
-            PC_sf_shap_df = pd.DataFrame(PC_sf_shap, columns=train_X.columns)
-            Z = linkage(PC_sf_shap.T, method='ward')
-            dendrogram(Z, labels=PC_sf_shap_df.columns.tolist())
-            maxxluster_threshold = 11 
-            cluster_labels = fcluster(Z, maxxluster_threshold, criterion='maxclust')
-            clustered_data = PC_sf_shap_df.copy()
-            clustered_data.loc['Cluster'] = cluster_labels
-            clustered_data_pc = clustered_data.loc['Cluster']
-            cluster_list = [clustered_data_pc.index[clustered_data_pc == clusterid].tolist() for clusterid in clustered_data_pc.unique()]
-            for clist in cluster_list:
-                for sf1 in clist:
-                    for sf2 in clist:
-                        if sf1 != sf2:
-                            adj_matrix_whole_df.loc[sf1][sf2] += 1
+        final_model_fit = final_model_custom
+        train_data = train_X
         
         # Save results to a JSON file
         fit = {
             "model_best_fit_param": best_params_custom,
-            "fit_RMSE": final_rmse_custom,
-            "fit_adj_matrix": adj_matrix_whole_df.to_dict(orient="split")
+            "fit_RMSE": final_rmse_custom
         }
         
         event_data = {event_name: fit}
@@ -102,15 +70,22 @@ def xgboostnet(event_name: str) -> Tuple[dict, float, pd.DataFrame]:
         data_path = data_dir_path(subdir="network")
         file_name = f"{event_name}_model_fit_results.json"
         whole_save_path = os.path.join(data_path, file_name)
+        
+        # Save JSON data
         with open(whole_save_path, "w") as f:
             json.dump(event_data, f)
 
-        return best_params_custom, final_rmse_custom, adj_matrix_whole_df
+        return best_params_custom, final_rmse_custom
 
     except requests.RequestException as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An error occurred while making a request: {e}"
+        )
+    except KeyError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Unexpected data format: {e}"
         )
     except Exception as e:
         raise HTTPException(
