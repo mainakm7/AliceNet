b11c0fb625f03da6f5646656cb2e6d2f15dbc71a
Mainak Mustafi <mainak.mustafi@gmail.com>
2024-08-05 15:39:33 -0400

    Added a checkbox for melting raw MI dataframe

--- home.py
+++ home.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 st.header("Welcome to AliceNet")
 
-for key in ['exp_dict', 'event_dict', 'mi_dict','mi_melt_df']:
+for key in ['exp_dict', 'event_dict', 'mi_dict','mi_melted_dict']:
     if key not in st.session_state.keys():
         st.session_state[key] = None
--- pages/02_Mutual_info_regression.py
+++ pages/02_Mutual_info_regression.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 # Ensure DataFrames are available in session_state
-if st.session_state.exp_dict is not None and st.session_state.event_dict is not None:
+if 'exp_dict' in st.session_state and 'event_dict' in st.session_state:
     exp_df = st.session_state.exp_dict
     event_df = st.session_state.event_dict
 
@@ -16,20 +16,19 @@ if st.session_state.exp_dict is not None and st.session_state.event_dict is not 
             response = requests.post("http://localhost:8000/mi/compute_mi", json={"sf_exp_df": exp_df, "sf_events_df": event_df})
             response.raise_for_status()
             data = response.json()
-            
+
             mi_dict = data["raw_mi_data"]
             mi_df = pd.DataFrame(mi_dict["data"], columns=mi_dict["columns"], index=mi_dict["index"])
-            
+
             # Write mi_df to session_state
             st.session_state._temp_mi_dict = mi_dict
-            
+
             with st.expander("Raw MI Dataframe:"):
                 st.write(mi_df)
         except requests.RequestException as e:
             st.error(f"Error fetching MI data: {e}")
 else:
     st.write("Expression and event dataframes are not available. First load them!")
-
 
 # Subdirectory Input
 subdir = st.text_input("Enter subdirectory", value="MI")
@@ -62,24 +61,22 @@ def load_mi_data(filename):
         return mi_dict
     except requests.RequestException as e:
         st.error(f"Error loading MI data: {str(e)}")
+        return {}
 
-def melt_raw_mi():
-    if "mi_df" in st.session_state:
-        mi_dict = st.session_state._temp_mi_dict
-        try:
-            response = requests.post("http://localhost:8000/mi/melt_mi", json={"mi_raw_data": mi_dict})
-            response.raise_for_status()
-            mi_melted_dict = response.json()
+def melt_raw_mi(mi_dict):
+    try:
+        response = requests.post("http://localhost:8000/mi/melt_mi", json={"mi_raw_data": mi_dict})
+        response.raise_for_status()
+        mi_melted_dict = response.json()
 
-            mi_melted_df = pd.DataFrame(mi_melted_dict["data"], columns=mi_melted_dict["columns"], index=mi_melted_dict["index"])
-            if not mi_melted_df.empty:
-                st.session_state.mi_melt_df = mi_melted_df
-                st.success("MI data melted successfully.")
-                requests.post("http://localhost:8000/mi/melted_mi_data_to_db", json={"mi_melted_data": mi_melted_df.to_dict(orient="split")})
-        except requests.RequestException as e:
-            st.error(f"Error in melting MI raw DataFrame: {str(e)}")
-    else:
-        st.error("No raw MI data available to melt.")
+        mi_melted_df = pd.DataFrame(mi_melted_dict["data"], columns=mi_melted_dict["columns"], index=mi_melted_dict["index"])
+        if not mi_melted_df.empty:
+            st.success("MI data melted successfully.")
+            requests.post("http://localhost:8000/mi/melted_mi_data_to_db", json={"mi_melted_data": mi_melted_df.to_dict(orient="split")})
+        return mi_melted_dict
+    except requests.RequestException as e:
+        st.error(f"Error in melting MI raw DataFrame: {str(e)}")
+        return {}
 
 # Selectbox for files
 filenames = fetch_file_list(subdir)
@@ -87,6 +84,7 @@ MI_file = st.selectbox("Select raw MI file", filenames if filenames else ["No fi
 
 # Load Button for MI Data
 load_btn = st.button("Load MI Data", type="primary")
+melt_check = st.checkbox("Melt DataFrame after loading")
 if load_btn:
     with st.expander("Raw MI Dataframe:"):
         if MI_file and MI_file != "No files available":
@@ -95,11 +93,16 @@ if load_btn:
             mi_df = pd.DataFrame(mi_dict['data'], columns=mi_dict['columns'], index=mi_dict['index'])
             if not mi_df.empty:
                 st.write(mi_df)
-                melt_raw_mi()
+                if '_temp_mi_dict' in st.session_state and melt_check:
+                    mi_melted_dict = melt_raw_mi(st.session_state._temp_mi_dict)
+                    st.session_state._temp_mi_melted_dict = mi_melted_dict
+                else:
+                    st.error("No MI dataframe to melt")
             else:
                 st.write("No data available.")
-            
         else:
             st.write("Select a valid MI file to display.")
 
-st.session_state.mi_dict = st.session_state._temp_mi_dict
+# Ensure session_state variables are updated
+st.session_state.mi_dict = st.session_state.get('_temp_mi_dict', {})
+st.session_state.mi_melted_dict = st.session_state.get('_temp_mi_melted_dict', {})
